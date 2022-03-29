5e8cc376987708e324942691659641747e2e9840
mickedance <mickedance@hotmail.com>
2022-03-29 10:59:36 +0200

    Ex1

---
+++ src/main/java/org/example/Ex1.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+        int[] arr = {11,23,39,55};
+
+        for(int nr: arr){
+            System.out.print(nr+" ");
+        }
+    }
+}
